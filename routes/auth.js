97534a42ace0c7c61f23b93c4e33d4fe7cdf9c3f
yuyasat <takiyour@gmail.com>
2023-04-27 12:02:55 +0900

    hash password and add user object to array

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { body, validationResult } = require("express-validator");
 const { User } = require("../db/User");
+const bcrypt = require("bcrypt");
 
 router.get("/", (req, res) => {
   res.send("Hello auth!");
@@ -10,7 +11,7 @@ router.post(
   "/register",
   body("email").isEmail(),
   body("password").isLength({ min: 6 }),
-  (req, res) => {
+  async (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
@@ -23,6 +24,10 @@ router.post(
     if (user) {
       return res.status(400).json([{ message: "ユーザが既に存在します" }]);
     }
+
+    let hashedPassword = await bcrypt.hash(password, 10);
+
+    User.push({ email, password: hashedPassword });
   }
 );
 
